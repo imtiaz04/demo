f45812986419ccde098a683028b9b47a849cb188
imtiaz04 <85178565+imtiaz04@users.noreply.github.com>
2025-05-16 16:21:24 -0400

    Update test_app.py

--- test_app.py
+++ test_app.py
@@ -1,13 +1,9 @@
 import unittest
-
 from app import greet
 
 class TestGreet(unittest.TestCase):
-
     def test_greet(self):
-
         self.assertEqual(greet("World"), "Hello, World!")
 
 if name == "__main__":
-
     unittest.main()
